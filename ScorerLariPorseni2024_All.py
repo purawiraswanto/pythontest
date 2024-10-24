b65f92e60e5fa5425ea54e2b61efc4332113bd39
purawiraswanto <67382087+purawiraswanto@users.noreply.github.com>
2024-10-24 21:30:58 +0700

    Update ScorerLariPorseni2024_All.py

--- ScorerLariPorseni2024_All.py
+++ ScorerLariPorseni2024_All.py
@@ -1,9 +1,13 @@
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
  
 # Load the CSV file into a DataFrame
 # df = pd.read_csv('c:/temp/ScorerLariPorseni2024_All.csv')
-df = pd.read_csv('https://github.com/purawiraswanto/pythontest/blob/main/ScorerLariPorseni2024_All.csv') 
+url = 'https://raw.githubusercontent.com/purawiraswanto/pythontest/main/ScorerLariPorseni2024_All.csv'
+df = pd.read_csv(url) 
  
 df['Time'] = pd.to_timedelta(df['Time'])
  
@@ -13,7 +17,7 @@ df['Minutes'] = df['Time'].dt.total_seconds() / 60
  
 teams = df['Team'].unique()
 genders = df['Gender'].unique()
- 
+
  
 plt.figure(figsize=(12, 8))
  
@@ -23,6 +27,9 @@ for team in teams:
         plt.plot(team_gender_data['Finisher'], team_gender_data['Minutes'], marker='o', linestyle='-', label=f"{team} - {gender}")
  
 plt.xlabel('Finisher Position')
+plt.xscale('linear')
+plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(1))
+plt.grid
 plt.ylabel('Time (minutes)')
 plt.title('Finisher Position vs Time for Each Team and Gender')
 plt.legend()
